c9dcb951768525b68d154b3a968399da2a9c6f41
Alexander <lapseofreason17@gmail.com>
2022-05-07 18:42:12 +0300

    Update 12.c

--- exercises/02_Representing_and_Manipulating_Information/12.c
+++ exercises/02_Representing_and_Manipulating_Information/12.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	  int x = 0x87654321;
+    int x = 0x87654321;
 	
     printf("The least significant byte of x, with all other bits set to 0: %X\n", x & 0x000000FF);
     printf("All but the least significant byte of x complemented, with the least significant byte left unchanged: %X\n", (~x & 0xFFFFFF00) | (x & 0x000000FF));
